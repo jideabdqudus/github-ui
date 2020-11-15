464057affd3317cd74ba9ce8efcc3e7cf09f5edb
jideabdqudus <jideabdqudus@gmail.com>
2020-11-15 23:35:51 +0100

    Added Commit

---
+++ src/components/TheProfile.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Avatar from "../assets/Avatar.png";
+import "./Components.css";
+import {
+  TeamOutlined,
+  StarOutlined,
+  PushpinOutlined,
+  LinkOutlined,
+  HeatMapOutlined,
+  FacebookOutlined,
+} from "@ant-design/icons";
+import { Divider, Row, Col, Button } from "antd";
+const TheProfile = () => {
+  return (
+    <div>
+      <div style={{ paddingRight: "20px" }}>
+        {" "}
+        <img src={Avatar} alt="Avatar" className="avatar" />
+        <div>
+          <h1 className="firstName">Jide Abdul-Qudus.</h1>
+          <h2 className="userName">jideabdqudus</h2>
+          <div>
+            <h4 className="bio">
+              Software Developer (MERN). I build web apps with API-centric
+              backends. Tech Junkie with penchant for design.
+            </h4>
+          </div>
+          <Button block>Edit Profile</Button>
+        </div>
+      </div>
+      <Row>
+        <TeamOutlined />
+        12 followers . 42 following
+        <StarOutlined />
+        10
+      </Row>
+      <PushpinOutlined /> Lagos, Nigeria
+      <LinkOutlined /> www.abdulqudus.com
+      <Divider />
+      <h4>Highlights</h4>
+      <HeatMapOutlined /> Arctic Code Vault Contributor
+      <Divider />
+      <h4>Organizations</h4>
+      <FacebookOutlined />
+    </div>
+  );
+};
+
+export default TheProfile;
--- src/components/TheRepos.js
+++ src/components/TheRepos.js
@@ -1,55 +1,34 @@
 import { Divider, Input, Row, Col, Button } from "antd";
 import React from "react";
 import TheRepo from "./TheRepo";
-import Avatar from "../assets/Avatar.png";
 import "./Components.css";
-import {
-  TeamOutlined,
-  StarOutlined,
-  PushpinOutlined,
-  LinkOutlined,
-  HeatMapOutlined,
-  FacebookOutlined,
-} from "@ant-design/icons";
+
+import TheProfile from "./TheProfile";
 
 const TheRepos = () => {
   return (
     <div className="container">
       <Row>
         <Col xs={6}>
-          <div style={{ paddingRight: "20px" }}>
-            {" "}
-            <img src={Avatar} alt="Avatar" className="avatar" />
-            <div>
-              <h1>Jide Abdul-Qudus</h1>
-              <h2>jideabdqudus</h2>
-              <div>
-                <h4>
-                  Software Developer (MERN). I build web apps with API-centric
-                  backends. Tech Junkie with penchant for design.
-                </h4>
-              </div>
-              <Button block>Edit Profile</Button>
-            </div>
-          </div>
-          <Row>
-            <TeamOutlined />
-            12 followers . 42 following
-            <StarOutlined />
-            10
-          </Row>
-          <PushpinOutlined /> Lagos, Nigeria
-          <LinkOutlined /> www.abdulqudus.com
-          <Divider />
-          <h4>Highlights</h4>
-          <HeatMapOutlined /> Arctic Code Vault Contributor
-          <Divider />
-          <h4>Organizations</h4>
-          <FacebookOutlined />
+          <TheProfile/>
         </Col>
 
         <Col xs={18}>
-          <Input placeholder="Find a repository" />
+          <Row>
+            <Col xs={18}>
+              {" "}
+              <Input placeholder="Find a repository" />
+            </Col>
+            <Col xs={2}>
+              <Button>Type: All</Button>
+            </Col>
+            <Col xs={2}>
+              <Button>Language: All</Button>
+            </Col>
+            <Col xs={2}>
+              <Button>New</Button>
+            </Col>
+          </Row>
           <Divider />
           <h3>124 results from public repositories</h3>
           <TheRepo />
